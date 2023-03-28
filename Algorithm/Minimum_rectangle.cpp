fedbef773316995b5be99845428e914494dd292e
ponomarev <ponomarev@radioavionica.ru>
2023-03-28 19:01:33 +0300

    Initial conditional for task8 Minimum Restangle

---
+++ Algorithm/Minimum_rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int minimum_restangle()
+{
+	cout << "here" << endl;
+	return 0;
+}
+
+int test_minimun_restangle()
+{
+	minimum_restangle();
+	return 0;
+}
