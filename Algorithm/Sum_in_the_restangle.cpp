9634add281fd6020e105c80e2875b4611df508d3
Alexander_laptop <ya-alexander@outlook.com>
2023-03-31 03:25:16 +0300

    Добавил матрицу ответов очень по тупому. В итоге работает еще медленней

--- Algorithm/Sum_in_the_restangle.cpp
+++ Algorithm/Sum_in_the_restangle.cpp
@@ -4,8 +4,13 @@
 
 using namespace std;
 
-int sum_in_the_rastangle(auto& matrix, int i_min, int i_max, int j_min, int j_max)
+
+
+
+int sum_in_the_rastangle(auto& matrix, int i_min, int j_min, int i_max, int j_max)
 {
+
+
 	int result = 0;
 	for (int i = i_min - 1; i < i_max; ++i)
 	{
@@ -30,22 +35,43 @@ int test_sum_in_the_rastangle()
 	size_t n_requests; input >> n_requests;
 	vector<vector<int>> matrix;
 	matrix.resize(N);
-	for (size_t i = 0; i < N; ++i)
+	for (size_t i = 0; i < matrix.size(); ++i)
 	{
 		matrix[i].resize(M);
-		for (size_t j = 0; j < M; ++j)
+		for (size_t j = 0; j < matrix[0].size(); ++j)
 		{
 			int p; input >> p;
 			matrix[i][j] = p;
 		}
 	}
+
+	vector<vector<vector<vector<int>>>> result_matrix;
+
+	result_matrix.resize(matrix.size());
+	for (size_t i = 0; i < matrix.size(); ++i)
+	{
+		result_matrix[i].resize(matrix[0].size());
+		for (size_t j = 0; j < matrix[0].size(); ++j)
+		{
+			result_matrix[i][j].resize(matrix.size());
+			for (size_t k = 0; k < matrix.size(); ++k)
+			{
+				result_matrix[i][j][k].resize(matrix[0].size());
+				for (size_t l = 0; l < matrix[0].size(); ++l)
+					result_matrix[i][j][k][l] = sum_in_the_rastangle(matrix, i + 1, j + 1, k + 1, l + 1);
+			}
+		}
+	}//*/
+
+
 	
 	for(size_t i = 0; i < n_requests; ++i)
 	{
 		int i_min, i_max, j_min, j_max;
 		input >> i_min; input >> j_min;
 		input >> i_max; input >> j_max;
-		cout << sum_in_the_rastangle(matrix, i_min, i_max, j_min, j_max) << endl;
+		//cout << sum_in_the_rastangle(matrix, i_min, j_min, i_max, j_max) << endl;
+		cout << result_matrix[i_min - 1][j_min - 1][i_max - 1][j_max - 1] << endl;
 	}
 	input.close();
 	return 0;
